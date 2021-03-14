b0201658f17527dda3597ddaad49d7f085464274
alireza-constantin <alirezasoheili8080@yahoo.com>
2021-03-14 15:16:03 +0330

    add edit to products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,20 +1,45 @@
 const Product = require('../models/product');
 
 exports.getAddProducts = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     isActive: true,
     path: '/admin/add-product',
+    editing: false,
   });
 };
 
 exports.postAddProducts = (req, res, next) => {
   const { title, imageUrl, price, description } = req.body;
-  const products = new Product(title, imageUrl, price, description);
+  const products = new Product(null, title, imageUrl, description, price);
   products.save();
   res.redirect('/');
 };
 
+exports.getEditProducts = (req, res, next) => {
+  const prodId = req.params.productId;
+  const editing = req.query.edit;
+  if (!editing) {
+    return res.redirect('/');
+  }
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      product: product,
+      pageTitle: 'Edit Product',
+      path: 'admin/edit-product',
+      editing,
+    });
+  });
+};
+exports.postEditProduct = (req, res, next) => {
+  const { productId, title, imageUrl, price, description } = req.body;
+  const products = new Product(productId, title, imageUrl, description, price);
+  products.save();
+  res.redirect('/admin/products');
+};
 exports.getAdminProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render('admin/products', {
--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,8 @@ const {
   getAddProducts,
   postAddProducts,
   getAdminProducts,
+  getEditProducts,
+  postEditProduct,
 } = require('../controllers/admin');
 
 // -----------------------------------------------Initit Router
@@ -14,5 +16,6 @@ const Router = express.Router();
 // ------------------------------------------------routes
 Router.route('/add-product').get(getAddProducts).post(postAddProducts);
 Router.route('/products').get(getAdminProducts);
-
+Router.get('/edit-product/:productId', getEditProducts);
+Router.post('/edit-product', postEditProduct);
 module.exports = Router;
