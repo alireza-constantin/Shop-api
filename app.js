5ae92d8c4ec1866ff2b19e5a0dbad299ccf4ea8d
alireza-constantin <alirezasoheili8080@yahoo.com>
2021-03-19 14:23:17 +0330

    add User model and relation with Product

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const ejs = require('ejs');
 //------------------------------------------------ Local and Core Module
 const path = require('path');
 const mysql = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 //------------------------------------------------ Initial express
 const app = express();
 
@@ -25,6 +27,14 @@ const sequelize = require('./util/database');
 // ------------------------------------------------------Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 // ------------------------------------------------------Using routes
 app.use('/admin', adminRouter);
 app.use(shop);
@@ -34,10 +44,23 @@ app.use((req, res, next) => {
   res.status(404).render('404', { pageTitle: 'Not Found', path: '/404' });
 });
 
+// -------------------------------------------------------Relating Database
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 // -------------------------------------------------------Init Database
 sequelize
   .sync()
   .then((res) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: 'Alireza', email: 'alireza@yahoo.com' });
+    }
+    return user;
+  })
+  .then((user) => {
     //------------------------------------------------------- Starting Server
     const PORT = 3000 || process.env.PORT;
     app.listen(PORT, () =>
---
+++ models/user.js
@@ -0,0 +1,27 @@
+const sequelize = require('../util/database');
+
+const Sequelize = require('sequelize');
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    validate: {
+      is: [
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+      ],
+    },
+  },
+});
+
+module.exports = User;
