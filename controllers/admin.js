ce1ada5a60ab7a43c17f622feb9ef59f380a0f93
alireza-constantin <alirezasoheili8080@yahoo.com>
2021-03-22 15:18:09 +0430

    update and delete -mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,19 +3,15 @@ const asyncHandler = require('../util/asyncHandler');
 
 //  @Method   Get Admin Products
 //  @Route    /admin/products
-exports.getAdminProducts = (req, res, next) => {
-  req.user
-    .getProducts()
-    .then((products) => {
-      res.render('admin/products', {
-        pageTitle: 'Admin Products',
-        prods: products,
-        isActive: true,
-        path: '/admin/products',
-      });
-    })
-    .catch((err) => console.log(err));
-};
+exports.getAdminProducts = asyncHandler(async (req, res, next) => {
+  const products = await Product.find();
+  res.render('admin/products', {
+    pageTitle: 'Admin Products',
+    prods: products,
+    isActive: true,
+    path: '/admin/products',
+  });
+});
 
 //  @Method   Get Add Products
 //  @Route    /admin/add-product
@@ -39,61 +35,47 @@ exports.postAddProducts = asyncHandler(async (req, res, next) => {
     description,
   });
   await product.save();
-  await res.redirect('admin/products');
+  await res.redirect('/admin/products');
 });
 
 //  @Method   Get Edit Products
 //  @Route    /admin/edit-product/:produtId
-exports.getEditProducts = (req, res, next) => {
+exports.getEditProducts = asyncHandler(async (req, res, next) => {
   const prodId = req.params.productId;
   const editing = req.query.edit;
   if (!editing) {
     return res.redirect('/');
   }
-  req.user
-    .getProducts({ where: { id: prodId } })
-    .then((product) => {
-      if (!product) {
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        product: product[0],
-        pageTitle: 'Edit Product',
-        path: 'admin/edit-product',
-        editing,
-      });
-    })
-    .catch((err) => console.log(err));
-};
+  const product = await Product.findById(prodId);
+  if (!product) {
+    return res.redirect('/');
+  }
+  res.render('admin/edit-product', {
+    product: product,
+    pageTitle: 'Edit Product',
+    path: 'admin/edit-product',
+    editing,
+  });
+});
 
 //  @Method   POST Edit Products
 //  @Route    /admin/edit-product
-exports.postEditProduct = (req, res, next) => {
+exports.postEditProduct = asyncHandler(async (req, res, next) => {
   const { productId, title, imageUrl, price, description } = req.body;
-  Product.findByPk(productId)
-    .then((product) => {
-      product.title = title;
-      product.imageUrl = imageUrl;
-      product.price = price;
-      product.description = description;
-      return product.save();
-    })
-    .then((result) => {
-      res.redirect('/admin/products');
-    })
-    .catch((err) => console.log(err));
-};
+  const product = await Product.findById(productId);
+  product.title = title;
+  product.imageUrl = imageUrl;
+  product.price = price;
+  product.description = description;
+  await product.save();
+  await res.redirect('/admin/products');
+});
 
 //  @Method   POST Delete Products
 //  @Route    /admin/delete-product
-exports.deleteProducts = (req, res, next) => {
+exports.deleteProducts = asyncHandler(async (req, res, next) => {
   prodId = req.body.productId;
-  Product.findByPk(prodId)
-    .then((product) => {
-      return product.destroy();
-    })
-    .then((product) => {
-      res.redirect('/admin/products');
-    })
-    .catch((err) => console.log(err));
-};
+  const product = await Product.findById(prodId);
+  await product.delete();
+  await res.redirect('/admin/products');
+});
