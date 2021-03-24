aa6c2f8164477d51e6c3c5f5d370c91d99e67f1e
alireza-constantin <alirezasoheili8080@yahoo.com>
2021-03-24 16:32:08 +0430

    added add to cart functionality

--- models/user.js
+++ models/user.js
@@ -25,4 +25,26 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.methods.addToCart = function (product) {
+  const cartIndex = this.cart.items.findIndex((p) => {
+    return p.productId.toString() === product._id.toString();
+  });
+  let newQty = 1;
+  const updatedCartItems = [...this.cart.items];
+  if (cartIndex >= 0) {
+    newQty = this.cart.items[cartIndex].quantity + 1;
+    updatedCartItems[cartIndex].quantity = newQty;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQty,
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
+
 module.exports = mongoose.model('User', userSchema);
